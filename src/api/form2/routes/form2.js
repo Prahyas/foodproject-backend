8a7ac5e20fc4af4e6c7744072b39a7a2edbb218d
prahyas <xrestha17@gmail.com>
2022-06-17 10:50:35 +0545

    added form 2

---
+++ src/api/form2/routes/form2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * form2 router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::form2.form2');
---
+++ src/api/form2/services/form2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * form2 service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::form2.form2');
